da37d9a4ba00ce8cc65a1b08935c5f1141670db2
kceusebio <kceusebio.wildflour@gmail.com>
2024-12-20 10:07:49 +0800

    Your first php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    // echo 'sample text....';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+        <h1>
+            <?='hello, ninjas..'; ?>
+        </h1>
+</body>
+</html>
