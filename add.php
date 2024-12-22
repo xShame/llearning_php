05130e403ec396f91d93424c89c0ecc711941ac2
kceusebio <kceusebio.wildflour@gmail.com>
2024-12-22 17:04:28 +0800

    Forms

---
+++ add.php
@@ -0,0 +1,41 @@
+<?php
+ 
+    // include('ninjas.php');
+    // require('ninjas.php');
+
+    // if(isset($_GET['submit'])){
+    //     echo $_GET['email'];
+    //     echo $_GET['title'];
+    //     echo $_GET['ingredients'];
+    // }
+    if(isset($_POST['submit'])){
+        echo $_POST['email'];
+        echo $_POST['title'];
+        echo $_POST['ingredients'];
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('templates/header.php'); ?>
+
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <!-- <form action="add.php" method="GET" class="white"> -->
+        <form action="add.php" method="POST" class="white">
+            <label for="">Your Email:</label>
+            <input type="text" name="email">
+            <label for="">Pizza Title:</label>
+            <input type="text" name="title">
+            <label for="">Ingredients (comma seperated):</label>
+            <input type="text" name="ingredients">
+
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+  
+    <?php include('templates/footer.php'); ?>
+</html>
