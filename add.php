ede94a124ad6ba8dafe9cb79b4ff24c5c9dfc6f6
kceusebio <kceusebio.wildflour@gmail.com>
2025-01-12 08:26:11 +0800

    showing errors

--- add.php
+++ add.php
@@ -8,38 +8,41 @@
     //     echo $_GET['title'];
     //     echo $_GET['ingredients'];
     // }
+    $title = $email = $ingredients = '';
+    $errors = array('email' => "", 'title' => "", 'ingredients' => "");
+
     if(isset($_POST['submit'])){
         // echo htmlspecialchars($_POST['email']);
         // echo htmlspecialchars($_POST['title']);
         // echo htmlspecialchars($_POST['ingredients']);
 
         if(empty($_POST['email'])){
-            echo "An email is required <br />";
+            $errors['email'] =  "An email is required <br />";
         } else{
             // echo htmlspecialchars($_POST['email']);
             $email = $_POST['email'];
 
             if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-                echo 'Email must be a valid email address <br />';
+                $errors['email'] = 'Email must be a valid email address <br />';
             } 
         }
         if(empty($_POST['title'])){
-            echo "A title is required <br />";
+            $errors['title'] =  "A title is required <br />";
         } else{
             // echo htmlspecialchars($_POST['title']);
             $title = $_POST['title'];
             if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
-				echo 'Title must be letters and spaces only <br />';
+				$errors['title'] = 'Title must be letters and spaces only <br />';
 			}
         }
 
         if(empty($_POST['ingredients'])){
-            echo "At least one ingredient is required <br />";
+            $errors['ingredients'] = "At least one ingredient is required <br />";
         } else{
             // echo htmlspecialchars($_POST['ingredients']);
             $ingredients = $_POST['ingredients'];
 			if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
-				echo 'Ingredients must be a comma separated list';
+				$errors['ingredients'] = 'Ingredients must be a comma separated list';
 			}
         }
     }
@@ -55,11 +58,20 @@
         <!-- <form action="add.php" method="GET" class="white"> -->
         <form action="add.php" method="POST" class="white">
             <label for="">Your Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?=htmlspecialchars($email);?>">
+            <div class="red-text">
+                <?=$errors['email']; ?>
+            </div>
             <label for="">Pizza Title:</label>
-            <input type="text" name="title">
+            <input type="text" name="title" value="<?=htmlspecialchars($title);?>">
+            <div class="red-text">
+                <?=$errors['title']; ?>
+            </div>
             <label for="">Ingredients (comma seperated):</label>
-            <input type="text" name="ingredients">
+            <input type="text" name="ingredients" value="<?=htmlspecialchars($ingredients);?>">
+            <div class="red-text">
+                <?=$errors['ingredients']; ?>
+            </div>
 
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
