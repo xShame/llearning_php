820420e01a2c1cb12a120489ac9cacab3549aa69
kceusebio <kceusebio.wildflour@gmail.com>
2025-01-26 08:26:57 +0800

    getting data from a database

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
         echo "Connection Error!: " . mysqli_connect_error();
     }
 
+    // write query for all pizzas
+    $sql = 'SELECT title, ingredients, id FROM pizzas';
+
+    // make query & get result
+   $result = mysqli_query($conn, $sql);
+
+    // fetch the resulting rows as an array
+   $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    //free the result from memory 
+    mysqli_free_result($result);
+
+    // close connection
+    mysqli_close($conn);
+
+    print_r($pizzas);
+
 ?>
 
 <!DOCTYPE html>
