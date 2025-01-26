4954c03ca5f6de468242121a9ed90235a8d4a421
kceusebio <kceusebio.wildflour@gmail.com>
2025-01-26 08:40:26 +0800

    rendering data from to a browser

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     }
 
     // write query for all pizzas
-    $sql = 'SELECT title, ingredients, id FROM pizzas';
+    $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
 
     // make query & get result
    $result = mysqli_query($conn, $sql);
@@ -27,13 +27,38 @@
     // close connection
     mysqli_close($conn);
 
-    print_r($pizzas);
+    // print_r($pizzas);
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
-  
+    
+    <h4 class="center grey-text">Pizzas!</h4>
+
+    <div class="container">
+        <div class="row">
+            <?php foreach($pizzas as $pizza) : ?>
+                <div class="col s6 md3">
+                    <div class="card z-depth-0">
+                        <div class="card-content center">
+                            <h6>
+                                <?=htmlspecialchars($pizza['title']); ?>
+                            </h6>
+                            <div>
+                                <?=htmlspecialchars($pizza['ingredients']); ?>
+                            </div>
+                        </div>
+                        <div class="card-action right-align">
+                                <a href="#" class="brand-text">
+                                    more info
+                                </a>
+                            </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+    </div>
     <?php include('templates/footer.php'); ?>
 </html>
